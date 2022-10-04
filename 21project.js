6df1cccdb69bd478bb9c83301be1551f3e56c655
dadasokeskar <dhirajkeskar2593@gmail.com>
2022-10-04 16:20:53 +0530

    firstc

---
+++ 21project.js
@@ -0,0 +1,63 @@
+//Operators
+let a=40;
+let b=20;
+let c1=a+b;
+let c2=a-b;
+let c3=a*b;
+let c4=a/b;
+let c5=a%b;
+console.log(c1,c2,c3,c4,c5);
+  
+//relational
+let a1=3;
+let b1=4
+
+console.log(3<4);
+console.log(5<4);
+
+console.log(3<=4);
+console.log(5<=4);
+
+console.log(7>=4);
+console.log(6>=6);
+console.log(5>=7);
+
+//logic operators
+let x1=7,y1=8,z1=9
+let result1=x1 < y1 && x1 < z1
+let n2="&& logical operator";
+console.log(n2);
+console.log(result1);
+let x=7,y=8,z=9
+let result12=x < y && x < z
+let n3="|| logical operator";
+console.log(n3);
+console.log(result12);
+
+//Assingment operator
+
+let n12="(=) Assignment operator =";
+let d1=10;
+d1+=5;
+console.log(n12,d1)
+
+//incremnet/decrement
+var number=8;
+let msg1="before increment=";
+
+console.log(msg1,number++);
+let msg2="after increment=";
+console.log(msg2,number);
+
+var number1=5;
+let msg12="before decrementcrement=";
+
+console.log(msg12,number1--);
+let msg22="after decrementt=";
+console.log(msg22,number1);
+
+//betwise operator
+let m="(&)Bit wise operator="
+console.log(m,1 | 2);
+
+
